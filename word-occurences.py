c5fef1c32134d296d9446b9c19b5af36b66c39d1
Manas Paranjape <manas@designX6.com>
2016-12-24 18:04:18 -0500

    Created function analyze_content.
    * In later commit, I will make this new function do its job, analyze the content

--- word-occurences.py
+++ word-occurences.py
@@ -1,5 +1,12 @@
 filename = input('Please type the document you want to result ')
-def load_dictionary():
-    f = open(filename)
-    f.read()
+def load_content(filename):
+    f = open(filename, "r")
+    content = f.read()
     f.close()
+    return content
+
+def analyze_content(content):
+    print(content)
+    
+content = load_content(filename)
+analyze_content(content)
