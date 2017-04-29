49dddd7f82163d5964ed5576ed6f2171aedc2238
mcode10 <manas@designx6.com>
2017-04-29 18:02:15 -0400

    Create dictionary and processed words (#3)
    * Getting progress on word list.
    
    * Implement load_dictionary.
    
    * Printed word, and the occurence.
    
    * Need to get rid of \ufeff showing up in a string.
    
    * Need to work on odd characters appearing in list.
    
    * Introduced lstrip, but having problems.
    
    * Program now strips punctuation.
    
    * Must make program non-case-sensitive.
    
    Fixes #1
    
    * Fixed #2
    
    * Does not allow strings that only have digits.

--- word-occurences.py
+++ word-occurences.py
@@ -1,12 +1,49 @@
-filename = input('Please type the document you want to result ')
+from string import punctuation
+filename = input('Please type in the document you want to result ')
 def load_content(filename):
     f = open(filename, "r")
     content = f.read()
     f.close()
     return content
 
+def load_dictionary(content):
+    my_list = content.split()
+##    list_words(my_list)
+##    strip_punctuation(my_list)
+    dictionary = dict()
+    count = len(my_list)
+    while count > 0:
+        count = count -1
+        word = my_list[count]
+        word2 = word.strip('!.*?:;,')
+        word = word2.lower()
+        if not word.isdigit():
+            if word in dictionary:
+                occurence = dictionary[word]
+                occurence = occurence +1
+                dictionary[word] = occurence
+                print(word, occurence)
+            else:
+                print("First occurence", word)               
+                dictionary[word] = 1
+##      print (dictionary)
+    return dictionary
+
+    
+def list_words(my_list):
+    count = len(my_list)
+    while count > 0:
+        count = count -1
+        word = my_list[count]
+##        if word.isalpha() is False:
+##           print(word)
+
 def analyze_content(content):
-    print(content)
+    dictionary = load_dictionary(content)
+##    for k,v in dictionary.items():
+##        print(k, v)
+##    print(dictionary)   
     
 content = load_content(filename)
 analyze_content(content)
+ 
