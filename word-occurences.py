61e94154f3616e4ac728794635c7b3289a78986f
mcode10 <manas@designx6.com>
2017-05-06 18:55:51 -0400

    Sortalphabetically to master (#4)
    * Program now prints word with the total occurence.
    
    * This commit cleans up the program.
    
    * Prompts user for a full path name.

--- word-occurences.py
+++ word-occurences.py
@@ -1,10 +1,15 @@
 from string import punctuation
-filename = input('Please type in the document you want to result ')
+filename = input('Please type in the document you want to result(please enter full file path) ')
+
+## load_content reads file specified above by user, then saves it in RAM.
+
 def load_content(filename):
     f = open(filename, "r")
     content = f.read()
     f.close()
     return content
+
+## Creates dictionary and finished list.
 
 def load_dictionary(content):
     my_list = content.split()
@@ -22,27 +27,16 @@ def load_dictionary(content):
                 occurence = dictionary[word]
                 occurence = occurence +1
                 dictionary[word] = occurence
-                print(word, occurence)
-            else:
-                print("First occurence", word)               
+            else:               
                 dictionary[word] = 1
-##      print (dictionary)
+    wordlist = sorted(dictionary.keys())
+    for aword in wordlist:
+         print(aword, dictionary[aword])
     return dictionary
 
-    
-def list_words(my_list):
-    count = len(my_list)
-    while count > 0:
-        count = count -1
-        word = my_list[count]
-##        if word.isalpha() is False:
-##           print(word)
-
+## Defines the dictionary that load_dictionary works with.
 def analyze_content(content):
     dictionary = load_dictionary(content)
-##    for k,v in dictionary.items():
-##        print(k, v)
-##    print(dictionary)   
     
 content = load_content(filename)
 analyze_content(content)
